8faf5a42157951f129e2632f63bc2c7bc6f7f0a6
Fabricio Hipólito <fabriciohipvpaul@gmail.com>
2025-10-29 09:26:28 -0300

    ta pronto pro online

--- banco.sql
+++ banco.sql
@@ -1,32 +1,78 @@
-CREATE DATABASE IF NOT EXISTS `agqoeafa_banco`;
-USE `agqoeafa_banco`;
+CREATE DATABASE IF NOT EXISTS `banco`;
+USE `banco`;
+
 
 DROP TABLE IF EXISTS `alunos`;
 CREATE TABLE IF NOT EXISTS `alunos` (
   `nome` varchar(100) NOT NULL,
   `imagem` varchar(100) NOT NULL,
-  `digital` int NOT NULL,
   `rm` int NOT NULL AUTO_INCREMENT,
   `entrada` enum('sim','nao','atrasado','') NOT NULL,
   PRIMARY KEY (`rm`)
+) AUTO_INCREMENT=23208;
+
+INSERT INTO `alunos` (`nome`, `imagem`, `rm`, `entrada`) VALUES
+('Pedro', '1', 23100, 'sim'),
+('Raul', '1', 23093, 'sim'),
+('Gabriel Lucas', '1', 23080, 'sim'),
+('Esaú Elias', '1', 23046, 'sim'),
+('Paulo', '', 23106, ''),
+('André de Pontes', '', 23128, ''),
+('Isaac Rogovski', '', 23002, ''),
+('André Santista', '', 23142, ''),
+('Fabricio Hipólito', '', 23201, ''),
+('Thayna', '', 23207, ''),
+('Gabriel Rocha', '', 23032, ''),
+('Luisa Yshizuka', '', 23198, ''),
+('Isaac 2', '', 23003, ''),
+('Felipe Alves', '', 23160, '');
+
+
+DROP TRIGGER IF EXISTS `after_aluno_insert`;
+DELIMITER $$
+CREATE TRIGGER `after_aluno_insert` AFTER INSERT ON `alunos` FOR EACH ROW INSERT INTO digitais (rm) VALUES (NEW.rm)
+$$
+DELIMITER ;
+
+DROP TABLE IF EXISTS `digitais`;
+CREATE TABLE IF NOT EXISTS `digitais` (
+  `rm` int NOT NULL,
+  `slot` int DEFAULT NULL,
+  PRIMARY KEY (`rm`)
 );
-INSERT INTO `alunos` (`nome`, `imagem`, `digital`, `rm`, `entrada`) VALUES
-('Pedro', '1', 1, 23100, 'sim'),
-('Raul', '1', 1, 23093, 'sim');
+
+
+INSERT INTO `digitais` (`rm`, `slot`) VALUES
+(23201, 2),
+(23002, 1),
+(23080, NULL),
+(23046, NULL),
+(23032, 5),
+(23093, NULL),
+(23100, NULL),
+(23106, NULL),
+(23128, NULL),
+(23142, NULL),
+(23207, NULL),
+(23198, 3),
+(23003, 4),
+(23160, 6);
 
 DROP TABLE IF EXISTS `funcionario`;
 CREATE TABLE IF NOT EXISTS `funcionario` (
   `nome` varchar(100) NOT NULL,
   `senha` varchar(255) NOT NULL,
-  `funcao` enum('adm','funcionario')NOT NULL,
+  `funcao` enum('adm','funcionario') NOT NULL,
   `id` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-);
+)AUTO_INCREMENT=10;
 
 
-INSERT INTO `teste` (`nome`, `senha`, `funcao`, `id`) VALUES
-('pedro', '$2y$10$gCfISVTQEKtuZ52Mq.leyuXfRp1lKBFjEsz2j5X4uylzegdO7jVb6', 'funcionario', 7),
-('Fabricio', '$2y$10$WtJq51YluwgkosCThJpz5eTLlkvFT9bUD0Cl90HlScvPaYoD8foWa', 'funcionario', 4),
-('Brenda', '$2y$10$LtBpDpdU1h/dI.fIRbEEiuW77PhNPsdS4ID2xirrGgmKJ67ddj2UG', 'adm', 5),
-('miguel', '$2y$10$wkIYd3A0gsAo1cwO6c0W5eYOlHaNfWNNh746INqTPkdGRqfUnjXb.', 'adm', 9);
+INSERT INTO `funcionario` (`nome`, `senha`, `funcao`, `id`) VALUES
+('Eduardo', '1234', 'funcionario', 1),
+('fabricio_1', '1234', 'adm', 6),
+('Faf', '1234', 'adm', 9),
+('pedro', '1234', 'funcionario', 5),
+('isaa', '1234', 'funcionario', 7),
+('isabe', '1234', 'adm', 8);
 COMMIT;
